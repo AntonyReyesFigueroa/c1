5abf01b31b0e7ffb06deff5025bab06152a48e3e
AntonyReyesFigueroa <antonyreyesfigueroa@gmail.com>
2024-04-06 18:29:48 -0500

    Primer commit

---
+++ scrpit.js
@@ -0,0 +1,14 @@
+const a = [1,2,3,4]
+
+console.log(a)
+
+const usuario = {
+    name: 'usuario',
+    age:23,
+    email:'usuario@gmail.com'
+}
+
+const alumno = {
+    name: 'nome',
+    age:23
+}
